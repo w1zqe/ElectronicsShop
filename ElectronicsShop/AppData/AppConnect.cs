d6ba51e3bab395933fdc53619a4399cd83595402
10221043 <10221043@D209PC17>
2025-05-27 12:43:18 +0300

    Добавьте файлы проекта.

---
+++ ElectronicsShop/AppData/AppConnect.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectronicsShop.AppData
+{
+    internal class AppConnect
+    {
+        public static ElectronicsShopEntities model0db; // Замените YourDbContext на ваш класс контекста
+        static AppConnect()
+        {
+            model0db = new ElectronicsShopEntities(); // Инициализация при первом обращении
+        }
+    }
+}
